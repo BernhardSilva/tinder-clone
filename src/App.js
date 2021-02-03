aae02d0f65dd7d21e18aa4f37727348269083294
BrnhardSilva <bernhard.silva@gmail.com>
2021-02-02 21:20:50 -0300

    Finished front header components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    //BEM clas naming convention
+    <div className="app">
+      <Header/>
+      {/* {TinderCards} */}
+      {/* {SwipeButtons} */}
+    </div>
+    );
+}
+
+export default App;
